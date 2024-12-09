ba35b390a845e4e7f8e23f8b7d066b2fcdf471fc
Maja Kądziołka <maya@compilercrim.es>
2024-12-09 17:13:16 +0100

    Day 7

---
+++ day7-part1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+total=0
+while IFS=: read target parts; do
+    parts=($parts)
+    options=(${parts[0]})
+    for part in ${parts[@]:1}; do
+        declare -A new=()
+        for opt in ${options[@]}; do
+            new[$((opt*part))]=
+            new[$((opt+part))]=
+        done
+        options=(${!new[@]})
+    done
+
+    for opt in ${options[@]}; do
+        if (( opt == target )); then
+            (( total += target ))
+            break
+        fi
+    done
+done
+
+echo $total
