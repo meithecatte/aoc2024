6675f3878641f30ca04e4a6e0bb1eb0c62501681
Maja Kądziołka <maya@compilercrim.es>
2024-12-17 14:44:00 +0100

    Day 11 part 2

--- day11.sh
+++ day11.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 LC_ALL=C
 
+declare -A cache=()
+
 doit() {
     local stone=$1 num_blinks=$2
     if (( num_blinks == 0 )); then
-        (( total++ ))
+        res=1
+        return
+    fi
+
+    if [[ -v cache["$stone $num_blinks"] ]]; then
+        res=${cache["$stone $num_blinks"]}
         return
     fi
 
@@ -12,18 +19,26 @@ doit() {
         doit 1 $((num_blinks - 1))
     elif (( ${#stone} % 2 == 0 )); then
         local k=$((${#stone} / 2))
-        doit ${stone:0:k} $((num_blinks - 1))
-        doit $((10#${stone:k})) $((num_blinks - 1))
+        doit ${stone:0:k} $((num_blinks - 1)); local left=$res
+        doit $((10#${stone:k})) $((num_blinks - 1)); local right=$res
+        (( res = left + right ))
     else
         doit $((stone * 2024)) $((num_blinks - 1))
     fi
+
+    cache["$stone $num_blinks"]=$res
 }
 
 read -a input
 
-total=0
+part1=0
+part2=0
 for stone in ${input[@]}; do
     doit $stone 25
+    (( part1 += res ))
+    doit $stone 75
+    (( part2 += res ))
 done
 
-echo $total
+echo $part1
+echo $part2
